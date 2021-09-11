efaf3a28ef6d8222aede2caeaed8c56802f49af8
prateek jain <jprateek07@gmail.com>
2021-09-11 17:44:58 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Layout from './components/layout'
+import About from './components/about'
+import RoundPlate from './components/roundPlate'
+import SquarePlate from './components/squarePlate'
+import HeartPlate from './components/heartPlate'
+import Spoons from './components/spoons'
+import Gallery from './components/gallery'
+import Contact from './components/contact'
+import HomePage from './components/homePage'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/round-plate' component={RoundPlate} />
+            <Route exact path='/square-plate' component={SquarePlate} />
+            <Route exact path='/heart-plate' component={HeartPlate} />
+            <Route exact path='/spoons' component={Spoons} />
+            <Route exact path='/gallery' component={Gallery} />
+            <Route exact path='/contact' component={Contact} />
+          </Switch>
+        </Router>
+      </Layout>
+    </>
   );
 }
 
