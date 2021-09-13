34263f1e31e7e2da3250d66b8b330b70c6fc9a54
prateek jain <jprateek07@gmail.com>
2021-09-13 11:34:18 +0530

    updated content

--- src/components/certificate.jsx
+++ src/components/certificate.jsx
@@ -11,63 +11,60 @@ function Certificate(props) {
     return (
         <div>
             <div class="banner">
-                <img src={bannerImg} class="img-responsive" alt="Areca Leaf Round Plate in Bangalore" title="Areca Leaf Round Plate in Bangalore"/>
-       </div>
+                <img src={bannerImg} class="img-responsive" alt="Areca Leaf Round Plate in Bangalore" title="Areca Leaf Round Plate in Bangalore" />
+            </div>
 
-                <div class="round-plate">
-                    <div class="container">
-                        <div class="row">
+            <div class="round-plate">
+                <div class="container">
+                    <div class="row">
 
-                            <div class="col-md-8">
-                                {/* <h1>Areca Leaf Round Plate in india India</h1> */}
-                            </div>
-
-                            <div class="col-md-4">
-                                <ul>
-                                    {/* <li class="active"><Link to="/"> <i class="fa fa-home"></i> Home</Link></li>
-                                    <li>Areca Leaf Round Plates</li> */}
-                                </ul>
-                            </div>
-
+                        <div class="col-md-8">
+                            {/* <h1>Areca Leaf Round Plate in india India</h1> */}
                         </div>
+
+                        <div class="col-md-4">
+                            <ul>
+                                {/* <li class="active"><Link to="/"> <i class="fa fa-home"></i> Home</Link></li>
+                                    <li>Areca Leaf Round Plates</li> */}
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
-                <section class="eco_service">
-                    <div class="container">
-                        <div class="row">
+            </div>
+            <section class="eco_service">
+                <div class="container">
+                    <div class="row">
                         <LeftNav />
-                            <div class="col-md-9">
-                                <div class="content_box">
-                                    <h2> Areca Leaf Round Plates</h2>
-                                    <div class="row">
-                                        <div class="col-md-5 round">
-                                            <img src={img1} class="img-responsive" alt="Areca Leaf 6 Inch Round Plate" title="Areca Leaf 6 Inch Round Plate" />
-                                            <p>Standard Round Plate 25cm <br/>25 x 1.5 cm deep<br/>Pack of 25</p>
+                        <div class="col-md-9">
+                            <div class="content_box certificate-section">
+                                <h2> Areca Leaf Round Plates</h2>
+                                <div class="row">
+                                    <div class="col-md-3 round">
+                                        <div className="certificate-icon my-3 ">
+                                        <i class="far fa-file-pdf fa-2x"></i>
                                         </div>
-
-                                        <div class="col-md-5 round">
-                                            <img src={img2} class="img-responsive" alt="Areca Leaf 8 Inch Round Plate" title="Areca Leaf 8 Inch Round Plate" />
-                                            <p>Standard Round Plate 20cm <br/>20x3cm deep<br/>Pack of 25</p>
+                                        <p>Certificate of Registration</p>
+                                    </div>
+                                    <div class="col-md-3 round">
+                                        <div className="certificate-icon my-3">
+                                        <i class="far fa-file-pdf fa-2x"></i>
                                         </div>
-
-
-                                        <div class="col-md-5 round">
-                                            <img src={img3} class="img-responsive" alt="Areca Leaf 10 Inch Round Plate" title="Areca Leaf 10 Inch Round Plate" />
-                                            <p>Standard Round Plate 15cm <br/>15 x 3 cm deep<br/>Pack of 25</p>
+                                        <p>Certificate of Importer-Exporter Code (IEC)</p>
+                                    </div>
+                                    <div class="col-md-3 round">
+                                        <div className="certificate-icon my-3">
+                                        <i class="far fa-file-pdf fa-2x"></i>
                                         </div>
-
-                                        <div class="col-md-5 round">
-                                            <img src={img4} class="img-responsive" alt="Areca Leaf 12 Inch Round Plate" title="Areca Leaf 12 Inch Round Plate" />
-                                            <p>Standard Round Plate 25cm <br/>25 x 2 cm deep<br/>Pack of 25</p>
-                                        </div>
-
+                                        <p>APEDA</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </section>
-            </div>
+                </div>
+            </section>
+        </div>
     );
 }
 
